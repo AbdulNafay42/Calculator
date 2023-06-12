352aec313626bac7e39bfec3ec5f8a022eb5d856
Abdul Nafay <103064913+AbdulNafay42@users.noreply.github.com>
2023-06-12 22:54:53 +0500

    Add files via upload

---
+++ Calculator/app2.js
@@ -0,0 +1,13 @@
+let input = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener("click" , (e) => {
+        if (e.target.innerHTML == "="){
+            string = eval(string)
+            input.value = string
+        }
+    })
+})
